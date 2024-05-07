7c95e1fd5b6d1a620ee7b8ea3da3e3408349790e
Yelhie <maelle.ew.m@gmail.com>
2024-05-07 18:00:45 +0200

    update README.md

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,32 @@
-# Print-It WebSite v.1
+# Print it
+
+Projet de la formation OpenClassrooms intégrateur web dont voici les objectifs:
+
+- Mise à jour du code HTML.
+- Intégration d'un carrousel en javascript.
+- Ajout d'event listener pour les flèches de navigation du caroussel.
+- Ajout de bullets points au caroussel.
+- Changement de slide au clique des bullets points.
+- Mise en place d'un défilement infini au carroussel.
+
+### Instructions
+
+- Lancer le projet avec liveserver
+
+## Démo
+
+[Démo Print it](https://yelhie.github.io/Print_it/)
+
+## Stack
+
+**Client:**
+
+- HTML
+- Css
+- JavaScript
+
+## Screenshots
+
+**Home page**
+
+![Print it home page](https://github.com/Yelhie/xi5ftcai605u/blob/master/img/printit_2024160855.jpg)
--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,88 @@
- const slides = [
- 	{
-  		"image":"slide1.jpg",
- 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
- 	},
-  	{
-  		"image":"slide2.jpg",
-  		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-  	},
-  	{
-  		"image":"slide3.jpg",
- 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-  	},
-  	{
- 		"image":"slide4.png",
- 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-  	}
- ]
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
- let currentSlide=0;
- const carrouselElementsLeftArrow = document.querySelector(".arrow_left");
- const carrouselElementsRightArrow = document.querySelector(".arrow_right");
- const carrouselElementsImage = document.querySelector(".banner-img");
- const carrouselElementsInfoE = document.querySelector(".banner-text");
- const carrouselElementsDotsContainer = document.querySelector(".dots");
- 
-carrouselElementsLeftArrow.addEventListener("click", function (){
-	carouselClick('gauche');
+let currentSlide = 0;
+const carrouselElementsLeftArrow = document.querySelector(".arrow_left");
+const carrouselElementsRightArrow = document.querySelector(".arrow_right");
+const carrouselElementsImage = document.querySelector(".banner-img");
+const carrouselElementsInfoE = document.querySelector(".banner-text");
+const carrouselElementsDotsContainer = document.querySelector(".dots");
+
+carrouselElementsLeftArrow.addEventListener("click", function () {
+  carouselClick("gauche");
 });
 
-carrouselElementsRightArrow.addEventListener("click", function (){
-	carouselClick('droite')
+carrouselElementsRightArrow.addEventListener("click", function () {
+  carouselClick("droite");
 });
 
 function creaDots() {
-	for (let i = 0; i < slides.length; i++) {
-	  let dot = document.createElement("span");
-	  dot.classList.add("dot");
-	  if (i === currentSlide) {
-		dot.classList.add("dot_selected");
-	  }
-	  dot.addEventListener("click", () => {
-		goToSlide(i);
-	  });
-	  carrouselElementsDotsContainer.appendChild(dot);
-	}
+  for (let i = 0; i < slides.length; i++) {
+    let dot = document.createElement("span");
+    dot.classList.add("dot");
+    if (i === currentSlide) {
+      dot.classList.add("dot_selected");
+    }
+    dot.addEventListener("click", () => {
+      goToSlide(i);
+    });
+    carrouselElementsDotsContainer.appendChild(dot);
   }
-
-function goToSlide(slideIndex) {
-	currentSlide = slideIndex;
-	updateCarousel();
-  }
-
-creaDots()
-
-function carouselClick(direction) {
-	if ('gauche' === direction) {
-		if(currentSlide<=0){
-			currentSlide = slides.length-1;
-		}else{
-			currentSlide--;
-		}
-	}
-	else {
-		if(currentSlide>=slides.length-1){
-			currentSlide = 0;
-		}else {
-			currentSlide++;
-		}
-	}
-	updateCarousel();
-  }
-  
-function updateCarousel() {
-	let dots = document.querySelectorAll(".dot");
-	dots.forEach((dot, index) => {
-	  if (index === currentSlide) {
-		dot.classList.add("dot_selected");
-	  } else {
-		dot.classList.remove("dot_selected");
-	  }
-	});	
-
-	let src = "./assets/images/slideshow/"+ slides[currentSlide].image;
-	carrouselElementsImage.setAttribute("src", src);
-
-	carrouselElementsInfoE.innerHTML = slides[currentSlide].tagLine;
-
 }
 
+function goToSlide(slideIndex) {
+  currentSlide = slideIndex;
+  updateDots();
+}
 
+creaDots();
+
+function carouselClick(direction) {
+  if ("gauche" === direction) {
+    if (currentSlide <= 0) {
+      currentSlide = slides.length - 1;
+    } else {
+      currentSlide--;
+    }
+  } else {
+    if (currentSlide >= slides.length - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+  }
+  updateDots();
+}
+
+function updateDots() {
+  let dots = document.querySelectorAll(".dot");
+  dots.forEach((dot, index) => {
+    if (index === currentSlide) {
+      dot.classList.add("dot_selected");
+    } else {
+      dot.classList.remove("dot_selected");
+    }
+  });
+
+  let src = "./assets/images/slideshow/" + slides[currentSlide].image;
+  carrouselElementsImage.setAttribute("src", src);
+
+  carrouselElementsInfoE.innerHTML = slides[currentSlide].tagLine;
+}
